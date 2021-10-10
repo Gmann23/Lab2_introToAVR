79e68cd4f6f281f2b654dfb2cdd04fb38acb67c0
Gurvinder Mann <Gurvindermann23@gmail.com>
2021-10-10 07:23:27 -0700

    completed part 2

--- test/tests.py
+++ test/tests.py
@@ -14,23 +14,28 @@
 # An example set of tests is shown below. It is important to note that these tests are not "unit tests" in 
 # that they are not ran in isolation but in the order shown and the state of the device is not reset or 
 # altered in between executions (unless preconditions are used).
-tests = [ {'description': 'PINA: 0x00 => PORTB: 0x00',
+tests = [ {'description': 'PINA: 0x00 => PORTC: 0x04',
     'steps': [ {'inputs': [('PINA',0x00)], 'iterations': 5 } ],
-    'expected': [('PORTB',0x00)],
+    'expected': [('PORTC',0x04)],
     },
-    {'description': 'PINA: 0x01 => PORTB: 0x01',
-    'steps': [ {'inputs': [('PINA',0x01)], 'iterations': 5 } ],
-    'expected': [('PORTB',0x01)],
-    },
-    {'description': 'PINA: 0x02 => PORTB: 0x02',
+ {'description': 'PINA: 0x02 => PORTC: 0x03',
     'steps': [ {'inputs': [('PINA',0x02)], 'iterations': 5 } ],
-    'expected': [('PORTB',0x00)],
+    'expected': [('PORTC',0x03)],
     },
-    {'description': 'PINA: 0x03 => PORTB: 0x00',
-	'steps': [{'inputs': [('PINA',0x03)], 'iterations': 5}],
-	'expected': [('PORTB',0x00)]
+
+    {'description': 'PINA: 0x03 => PORTC: 0x02',
+    'steps': [ {'inputs': [('PINA',0x03)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x02)],
+    },
+    {'description': 'PINA: 0x07 => PORTC: 0x02',
+    'steps': [ {'inputs': [('PINA',0x03)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x02)],
+    },
+    {'description': 'PINA: 0x0F => PORTC: 0x00',
+	'steps': [{'inputs': [('PINA',0x0F)], 'iterations': 5}],
+	'expected': [('PORTC',0x00)]
 	}
 
     ]
-#watch = ['PORTB']
+#watch = ['PORTC']
 
